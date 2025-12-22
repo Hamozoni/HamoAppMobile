701013d0920fd0d3e24e898e4304a657375a2ead
Mohamed Yahia <m.yahya199290@gmail.com>
2025-12-22 19:04:34 +0300

    modified:   app/(tabs)/chats/contacts.jsx         modified:   package-lock.json         modified:   package.json

--- app/(tabs)/chats/contacts.jsx
+++ app/(tabs)/chats/contacts.jsx
@@ -1,7 +1,47 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View, TouchableOpacity } from "react-native";
+import * as Contacts from "expo-contacts";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Contacts() {
-    return <View>
-        <Text>Contacts</Text>
-    </View>;
+export default function ContactsPage() {
+
+    const [contacts, setContacts] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Contacts.requestPermissionsAsync();
+            if (status === "granted") {
+                const { data } = await Contacts.getContactsAsync({ fields: [Contacts.Fields.PhoneNumbers] });
+                setContacts(data);
+                console.log(data[10].phoneNumbers);
+
+            }
+
+        })()
+    }, []);
+    return (
+        <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
+            <ScrollView contentInsetAdjustmentBehavior="automatic" style={{ flex: 1, paddingHorizontal: 10 }}>
+                <View>
+                    <TouchableOpacity>
+                        <Ionicons name="people-outline" size={26} color="black" />
+                        <Text>New Group</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name="person-add-outline" size={26} color="black" />
+                        <Text>New Contact</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name="people-outline" size={26} color="black" />
+                        <Text>New Community</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name="megaphone-outline" size={26} color="black" />
+                        <Text>New broadcast</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
