950c8f11d412eda0b1532119526403655e0bee0e
Mohamed Yahia <m.yahya199290@gmail.com>
2026-01-23 11:06:11 +0300

    modified:   app/_layout.tsx         modified:   app/index.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,38 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { queryClient } from "../lib/queryClient.config";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { ActivityIndicator } from "react-native";
+import { AuthBootstrap } from "../utils/authBootstrap";
 
 
 export default function RootLayout() {
 
+    const { hydrate, hydrated, user } = useAuthStore();
+
+    useEffect(() => {
+        hydrate();
+    }, []);
+
+    if (!hydrated) return <ActivityIndicator />;
 
     return (
-        <GestureHandlerRootView>
-            <QueryClientProvider client={queryClient}>
-                <Stack screenOptions={{ headerShown: false }}>
-                    <Stack.Screen name="(screens)" />
-                    <Stack.Screen name="(tabs)" />
-                    <Stack.Screen name="(auth)" />
-                </Stack>
-            </QueryClientProvider>
-        </GestureHandlerRootView>
+        <>
+            <AuthBootstrap />
+
+            {!user ? <Redirect href="/(auth)/login" /> :
+                <GestureHandlerRootView>
+                    <QueryClientProvider client={queryClient}>
+                        <Stack screenOptions={{ headerShown: false }}>
+                            <Stack.Screen name="(screens)" />
+                            <Stack.Screen name="(tabs)" />
+                            <Stack.Screen name="(auth)" />
+                        </Stack>
+                    </QueryClientProvider>
+                </GestureHandlerRootView>}
+        </>
     );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,6 @@
-import React, { useEffect } from "react";
-import { Redirect } from "expo-router";
+
 import { ActivityIndicator } from "react-native";
-import { useAuthStore } from "../store/useAuthStore";
 
 export default function Home() {
-    const { hydrate, hydrated, user } = useAuthStore();
-
-    useEffect(() => {
-        hydrate();
-    }, []);
-
-    if (!hydrated) {
-        // VERY short splash, not blank
-        return <ActivityIndicator />;
-    }
-
-    if (!user) {
-        return <Redirect href="/(auth)/login" />;
-    }
-
-    return <Redirect href="/(tabs)/chat" />;
+    return <ActivityIndicator />
 }
