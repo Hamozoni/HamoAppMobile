3b8906c78a22864429861f45380744b16bcf3b71
Mohamed Yahia <m.yahya199290@gmail.com>
2026-01-30 08:17:07 +0300

    modified:   db/services/syncContacts.service.ts

--- db/services/syncContacts.service.ts
+++ db/services/syncContacts.service.ts
@@ -1,9 +1,7 @@
 import * as Contacts from "expo-contacts";
-import { axiosInstance } from "../../lib/axios.config";
-// import { normalizePhone } from "@/utils/phoneNormalizer";
-// import { ContactsRepo } from "@/db/repositories/contacts.repo";
 
-export async function syncContacts(defaultCountry: string) {
+export async function syncContacts(defaultCountry: string = "SD") {
+
     // 1️⃣ Permission
     const { status } = await Contacts.requestPermissionsAsync();
     if (status !== "granted") {
@@ -16,7 +14,7 @@ export async function syncContacts(defaultCountry: string) {
         fields: [Contacts.Fields.PhoneNumbers],
     });
 
-    let conatcts: any[] = [];
+    const conatcts: any[] = [];
 
     data.map((contact) => {
         contact.phoneNumbers?.map(numb => {
@@ -24,47 +22,15 @@ export async function syncContacts(defaultCountry: string) {
         })
     })
 
-    console.log(conatcts)
 
-    //   if (!data.length) return;
 
-    //   const now = Date.now();
-    //   const normalizedSet = new Set<string>();
+    const phoneNumbers = conatcts.map(({ phoneNumber }) => phoneNumber)
 
-    //   // 3️⃣ Normalize & store locally
-    //   for (const contact of data) {
-    //     for (const phone of contact.phoneNumbers ?? []) {
-    //       const normalized = normalizePhone(phone.number, defaultCountry);
-    //       if (!normalized) continue;
+    const d = {
+        phoneNumbers,
+        Contacts
+    }
 
-    //       normalizedSet.add(normalized);
-
-    //       await ContactsRepo.insertOrIgnore(
-    //         normalized,
-    //         contact.name ?? "",
-    //         now
-    //       );
-    //     }
-    //   }
-
-    //   // 4️⃣ Send numbers to backend
-    //   const phoneNumbers = Array.from(normalizedSet);
-    //   if (!phoneNumbers.length) return;
-
-    //   const { data: matches } = await axiosInstance.post("/contacts/sync", {
-    //     phoneNumbers,
-    //   });
-
-    //   // matches: [{ phone, userId, displayName }]
-
-    //   // 5️⃣ Update registered contacts locally
-    //   for (const match of matches) {
-    //     await ContactsRepo.markAsRegistered(
-    //       match.phone,
-    //       match.userId
-    //     );
-    //   }
-
-    //   console.log("Contacts sync completed");
+    return d
 }
 
