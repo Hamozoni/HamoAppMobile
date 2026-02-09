b99bce33d9544ea3ddcda3718575e6eba31dcd05
Mohamed Yahia <m.yahya199290@gmail.com>
2026-02-09 21:48:06 +0300

    modified:   app/(screens)/chatWindow/profile.tsx

--- app/(screens)/chatWindow/profile.tsx
+++ app/(screens)/chatWindow/profile.tsx
@@ -1,15 +1,27 @@
-import { ScrollView, Text } from "react-native"
+import { Image, ScrollView, Text, View } from "react-native"
 import ThemedSafeAreaView from "../../../components/themedViews/safeAreaView"
 import { useLocalSearchParams, useSearchParams } from "expo-router/build/hooks"
+import { useContactsStore } from "../../../hooks/store/useContactsStore";
 
 
 export default function Profile() {
 
     const { phone } = useLocalSearchParams();
 
+    const { registered } = useContactsStore();
+
+    const findContact = () => {
+        return registered.find(e => e.phoneNumber === phone)
+    }
+
+
+
     return (
         <ThemedSafeAreaView>
             <ScrollView >
+                <View>
+                    <Image source={{ uri: findContact().profilePicture }} width={100} height={100} style={{ borderRadius: "50%" }} />
+                </View>
                 <Text>
                     Profile :{phone}
                 </Text>
