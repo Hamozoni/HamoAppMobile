6fbfd404aade978adc2f4b202b9eefc8c92e9cdd
Mohamed Yahia <m.yahya199290@gmail.com>
2026-02-10 15:25:01 +0300

    modified:   app/(screens)/chatWindow/profile.tsx         modified:   components/chats/chatHeader.tsx

--- app/(screens)/chatWindow/profile.tsx
+++ app/(screens)/chatWindow/profile.tsx
@@ -10,8 +10,12 @@ export default function Profile() {
 
     const { registered } = useContactsStore();
 
+
+
     const findContact = () => {
-        return registered?.find(e => e.phoneNumber === phone)
+
+        console.log(`+${phone}`)
+        return registered?.find(e => e.phoneNumber === `+${phone.replace(' ', '')}`)
     };
 
 
@@ -19,12 +23,18 @@ export default function Profile() {
     return (
         <ThemedSafeAreaView>
             <ScrollView >
-                <View style={{ justifyContent: "center", alignItems: 'center', paddingHorizontal: 20 }}>
-                    <Image source={{ uri: findContact()?.profilePicture }} width={100} height={100} style={{ borderRadius: "50%" }} />
+                <View style={{ justifyContent: "center", alignItems: 'center', paddingTop: 40, marginBottom: 20 }}>
+                    <Image source={{ uri: findContact()?.profilePicture }} width={150} height={150} style={{ borderRadius: "50%" }} />
                 </View>
-                <Text>
-                    Profile :{phone}
-                </Text>
+                <View style={{ flexDirection: "column", justifyContent: "center", alignContent: "center" }}>
+                    <Text style={{ fontSize: 22, fontWeight: 900, textAlign: 'center', marginBottom: 5 }}>
+                        {findContact()?.displayName}
+                    </Text>
+                    <Text style={{ fontSize: 18, textAlign: 'center', color: "gray" }}>
+                        {findContact()?.phoneNumber}
+                    </Text>
+
+                </View>
             </ScrollView>
         </ThemedSafeAreaView>
     )
--- components/chats/chatHeader.tsx
+++ components/chats/chatHeader.tsx
@@ -1,5 +1,4 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
-import { CHATS } from "../../constants/chats";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import React from "react";
