b79129205691b2b89da2257c7c3409f83bdbf3ea
Mohamed Yahia <m.yahya199290@gmail.com>
2026-06-26 18:49:17 +0300

    modified:   .env         modified:   lib/axios.config.ts

--- lib/axios.config.ts
+++ lib/axios.config.ts
@@ -21,7 +21,7 @@ declare module "axios" {
  */
 
 export const axiosInstance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8001/api',
+    baseURL: process.env.EXPO_PUBLIC_API_URL,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
